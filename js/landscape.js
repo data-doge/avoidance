b2773199df65562299d5039089bb0e83d4f7c059
euglazer <eugenestapler@gmail.com>
2016-01-05 19:06:56 +1300

    refactor landscape js

--- js/landscape.js
+++ js/landscape.js
@@ -3,6 +3,7 @@ var Fixed2DArray = require('fixed-2d-array')
 var _ = require('lodash')
 var $ = require('jquery')
 var Bot = require('./bot')
+var randomInt = require('random-int')
 
 var Landscape = stampit({
   init: function () {
@@ -10,37 +11,19 @@ var Landscape = stampit({
     this.$botCounter = $('#bot-count')
     this.$collisionAvoidedCounter = $('#collisions-avoided-count')
     this.collisionsAvoided = 0
-    this.initializeGrid()
-    this.initializePotentialCoords()
+    this.grid = new Fixed2DArray(this.height, this.width, null)
+    this.bots = []
     this.initializeBots()
     this.render()
   },
   methods: {
-    initializeGrid: function () {
-      this.grid = new Fixed2DArray(this.height, this.width, null)
-    },
-    initializePotentialCoords: function () {
-      this.potentialCoords = []
-      var self = this
-      _.times(self.height, function (r) {
-        _.times(self.width, function (c) {
-          self.potentialCoords.push({r: r, c: c})
-        })
-      })
-    },
     initializeBots: function () {
-      var self = this
-      var numOfBots = parseInt(self.potentialCoords.length * this.densityPercent / 100)
-      var activeCoords = _.take(_.shuffle(self.potentialCoords), numOfBots)
-      this.bots = []
-      _.each(activeCoords, function (coord) {
-        self.addBot({c: coord.c, r: coord.r})
-      })
+      var numOfCells = this.width * this.height
+      var numOfBots = parseInt(numOfCells * this.densityPercent / 100)
+      _.times(numOfBots, this.addBot.bind(this))
     },
-    addBot: function (params) {
-      var self = this
-      params = params || _.sample(this.potentialCoords)
-      var bot = new Bot(_.merge(params, {scale: this.scale}))
+    addBot: function () {
+      var bot = Bot(_.merge(this.getRandCoords(), {scale: this.scale}))
       bot.landscape = this
       this.bots.push(bot)
       this.grid.set(bot.r, bot.c, bot)
@@ -70,6 +53,9 @@ var Landscape = stampit({
     },
     updateFrame: function () {
       _.each(this.bots, this.updatePositionFor.bind(this))
+    },
+    getRandCoords: function () {
+      return { r: randomInt(this.height - 1), c: randomInt(this.width - 1) }
     }
   }
 })
