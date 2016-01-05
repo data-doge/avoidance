ed3fe2c2ba019870cf068d389d08e5fa383d2d1c
euglazer <eugenestapler@gmail.com>
2016-01-05 19:29:18 +1300

    remove ruby prototyping stuff

--- prototyping/index.rb
+++
@@ -1,9 +0,0 @@
-require_relative "landscape.rb"
-
-landscape = Landscape.new(width: 50, height: 50, density_percent: 10)
-
-loop do
-  landscape.render_frame
-  landscape.prepare_frame
-  sleep(0.04)
-end
