0f3f1506d7bb80971c0b8b61fcbc14b1d5ac6523
euglazer <eugenestapler@gmail.com>
2016-01-06 14:04:38 +1300

    change defaults

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var $ = require('jquery')
 var Landscape = require('./landscape')
 
-var landscape = Landscape({width: 100, height: 100, densityPercent: 1, scale: 5})
+var landscape = Landscape({width: 40, height: 40, densityPercent: 10, scale: 15})
 
 $(document).on('keypress', function () { landscape.addBot() })
 
